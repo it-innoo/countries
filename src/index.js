2295c79f2aae8b6708acddecdf20b7bf84253fde
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-07-27 19:06:10 +0300

    Step 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h2>{country.name}</h2>
+
+      <div>capital {country.capital}</div>
+
+      <div>populaton {country.population}</div>
+
+      <h3>languages</h3>
+
+      <ul>
+        {country.languages.map(language =>
+          <li key={language.iso639_1}>{language.name}</li>
+        )}
+      </ul>
+
+      <div>
+        <img src={country.flag} alt='flag' height='100' />
+      </div>
+    </div>
+  )
+}
+
+const Countries = ({ countries }) => {
+  if (countries.length === 0) {
+    return <div>
+      No countries, specify another filter
+    </div>
+  }
+
+  if (countries.length === 1) {
+    return <Country country={countries[0]} />
+  }
+
+  if (countries.length > 10) {
+    return (
+      <p>Liikaa osumia, ole hyvä ja rajaa hakuehtoja!</p>
+    )
+  }
+
+  return (
+    <div>
+      {
+        countries.map(c =>
+          <div key={c.alpha3Code}>
+            {c.name}
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [search, setSearch] = useState('swi')
+  const endpoint = 'https://restcountries.eu/rest/v2/all'
+
+  useEffect(() => {
+    axios
+      .get(endpoint)
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleChange = (event) =>
+    setSearch(event.target.value)
+
+  const countriesToShow = search.length > 0
+    ? countries.filter(c => c.name.toLowerCase().includes(search.toLowerCase()))
+    : countries
+
+  return (
+    <div>
+      <div>
+        find countries
+         <input onChange={handleChange} value={search} />
+      </div>
+      <Countries
+        countries={countriesToShow}
+      />
+    </div>
+  )
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
